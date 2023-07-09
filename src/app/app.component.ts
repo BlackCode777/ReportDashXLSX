591bba50343286f9154e009c90a96fa4cece6140
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-09 17:24:07 -0300

    ...Atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ReportDashXLSX';
+  
+  /* variável ngModel para salvar os itens escolhidos no dropbox */
+  public selectedReport: any;
+
+  /* um array de itens de nomes de relatórios */
+  public reportsGroups: Array<any> = [
+    
+      { id: 1, name: 'Relatório de Vendas por Período' },
+      { id: 2, name: 'Relatório de Vendas por Produto' },
+      { id: 3, name: 'Relatório de Vendas por Cliente' },
+      { id: 4, name: 'Relatório de Vendas por Vendedor' }
+    
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DropdownModule } from 'primeng/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    DropdownModule,
     BrowserModule,
     AppRoutingModule
   ],
